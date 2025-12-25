e4bdf85349cb94d543942d7332789b65953838d1
m4rk4lfred <152765365+m4rk4lfred@users.noreply.github.com>
2025-12-25 19:59:01 +0800

    Added functionality

--- Frontend/src/Pages/LandingPage/Features2.jsx
+++ Frontend/src/Pages/LandingPage/Features2.jsx
@@ -2,9 +2,14 @@
 function Features2(){
    return(
    <>
-   <div className="h-screen w-full font-poppins tracking-widest text-lg font-bold mt-15">
-      <div className="flex justify-center items-center">
-         <h1 className="text-primary-dark ">Multichain Integration</h1>
+   <div className="h-screen w-full font-poppins tracking-widest text-lg font-bold mt-15 ">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+         <div className="w-auto h-auto  flex flex-col items-center justify-center px-7  md:pl-25 md:items-start">
+            <h1 className="text-primary-dark text-xl font-black tracking-[0.25em] md:text-5xl">Multichain Integration</h1>
+            <h2 className="font-bold text-sm mt-5 tracking-[0.070em] text-branding">Track Every Asset, Across Every Chain.</h2>
+            <p  className="text-center mt-5 font-normal text-sm leading-8 md:leading-10 text-[#737373] px-9 md:text-start md:px-0">Effortlessly track your wallet balances, NFT collections, and DeFi positions across 120+ blockchains in one centralized dashboard.</p>
+         </div>
+         <div className="bg-green-400">d</div>
       </div>
    </div>
     
